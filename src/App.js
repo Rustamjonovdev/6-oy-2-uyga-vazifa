756a369e04878fcaac7548fa4644710921ae50c6
Rustamjonov <rustamjonovshahzodbek0@gamil.com>
2024-04-18 19:35:27 +0500

    all ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const imageNames = ['./images/image-0', './images/image-1', './images/image-2']
 
 function App() {
+  const [counter, setCounter] = useState(imageNames.length - 1)
+  
+  const back = () => {
+    if(counter > 0){
+      setCounter(counter - 1);
+    } else  {
+      setCounter(imageNames.length - 1)
+    }
+  }
+  const next = () => {
+    if(counter < imageNames.length - 1 ){
+      setCounter(counter + 1);
+    } else  {
+      setCounter(0)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+    <img src={`./images/image-${counter}.jpg`} width={300} />
+    <button onClick={ () => back()}> Back </button>
+    <button onClick={ () => next()}> Next </button>
+   </div>
   );
 }
 
